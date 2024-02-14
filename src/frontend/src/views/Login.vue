4af8da89ffa0a06d487a86b7d66ddc886c168e51
Yousaf Anwar <yousafanwar74@gmail.com>
2024-02-15 01:24:35 +0400

    vue templete

---
+++ src/frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+    <main>
+        <input type="text" v-model="xyz">
+        <input type="text" v-model="xyz">
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            xyz: "mydata"
+        }
+    }
+    
+}
+</script>
