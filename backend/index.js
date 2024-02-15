99ff9eb1d8908cca07be8d7d86aed0ee96f7d70f
Yousaf Anwar <yousafanwar74@gmail.com>
2024-02-15 12:47:47 +0400

    login page created

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import config from './config.js';
 import userRoutes from './routes/user.js';
 import authRoutes from './routes/auth.js';
+import cors from 'cors';
 
 let app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api/', authRoutes);
 
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="card-panel login blue darken-2 white-text center">
 		<h2>Recruitzi Login</h2>
-		<form action="/" @submit.prevent="login()">
+		<form action="/" @submit.prevent="userLogin()">
 			<div class="input-field">
 				<i class="material-icons prefix">email</i>
-				<input type="email" id="email" class="white-text" />
+				<input type="email" id="email" class="white-text" v-model="email" />
 				<label for="email" class="white-text">Email</label>
 			</div>
 			<div class="input-field">
 				<i class="material-icons prefix">lock</i>
-				<input type="password" id="password" class="white-text" />
+				<input type="password" id="password" class="white-text" v-model="password" />
 				<label for="password" class="white-text">Password</label>
 			</div>
 			<input type="submit" value="Login" class="btn btn-large btn-extended grey lighten-4 black-text" />
@@ -18,12 +18,35 @@
 </template>
 
 <script>
-	import router from '../router/index';
-	export default {
-		methods: {
-			login() {
-				router.push('./');
+import router from '../router/index';
+export default {
+	data(){
+		return{
+			email: '',
+			password: ''
+		}
+	},
+	methods: {
+		async userLogin() {
+			try {
+				const response = await fetch('http://localhost:3000/api/login', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						email: this.email,
+						password: this.password
+					})
+				});
+				if (!response.ok) {
+					throw new Error('Failed to login');
+				}
+				const token = await response.json();
+				router.push('./'); 
+			} catch (error) {
+				this.errorMessage = 'Invalid email or password';
+				console.error('Error logging in:', error);
 			}
 		}
-	};
+	}
+};
 </script>
